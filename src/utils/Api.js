60eb3f071864f9b39b600b8f8d3ca43307793cf4
smarten <smarten.developer@gmail.com>
2020-11-11 15:58:07 -0800

    set up for deploy

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const queryUrl = "https://randomuser.me/api/?results=50&nat=us";
+const queryUrl = "https://randomuser.me/api/?results=100&nat=us";
 
 export default async function getUsers() {
   return await axios.get(queryUrl);
